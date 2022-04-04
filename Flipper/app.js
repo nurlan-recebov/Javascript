832c97a2aac3da9ab585affb670fd9554a524102
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-04-05 02:42:18 +0400

    yeni task

---
+++ Flipper/app.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.btn');
+const con = document.querySelector('.container');
+const bac = document.querySelector('.back');
+const arran = ["green", "red", "blue", "yellow", "black", "orange", "pink"]
+let i = 0;
+btn.addEventListener("click", function() {
+
+    if (i < arran.length) {
+        con.style.backgroundColor = arran[i - 1];
+        bac.textContent = arran[i];
+        bac.style.backgroundColor = arran[i];
+
+        i++
+
+    }
+
+})
+
+
+
+// function nurlan() {
+//     if (i < arran.length) {
+
+//         i++
+
+//     }
+// }
