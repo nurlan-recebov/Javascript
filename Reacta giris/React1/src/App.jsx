7e7b57c9d5a34f841e48493ba10f9fac672dddf7
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-07-25 17:00:25 +0400

    task

---
+++ Reacta giris/React1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+const App=(props)=> {
+  
+
+  return (
+    <div className="App">
+     <h3>{props.cardTitle}</h3>
+    </div>
+  )
+
+}
+
+export default App
---
+++ Reacta giris/React1/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='Main'>
+    <App cardTitle="Turkiye"> </App>
+    <App cardTitle="Azerbaycan"></App>
+    <App cardTitle="Rusiya"></App>
+    
+    
+  </div>
+)
