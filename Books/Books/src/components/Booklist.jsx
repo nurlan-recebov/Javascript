09aabab8cfba8cf767b595a8d2323d9b8b185965
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-08-12 18:14:48 +0400

    API

--- Books/Books/src/components/Booklist.jsx
+++ Books/Books/src/components/Booklist.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import Book from './Book';
-import './Booklist.css'
-class Booklist extends React.Component{
-render(){
-    const booklist=this.props.books.map((book,i)=>{
-        return <Book book={book} key={i}></Book>
-    })
-    return(
-        <section className="page-section"  id="portfolio">
-        <div className="container">
-            <div className="text-center">
-                <h2 className="section-heading text-uppercase">BookFolio</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-              
-            </div>
-            <div className="row">
-
-              {booklist}
-
-            </div>
-        </div>
-    </section>
+import React from "react";
+import Book from "./Book";
+import "./Booklist.css";
+import {BookContext} from "../contexts/BookContext";
+class Booklist extends React.Component {
+  
+  render() {
  
-
+    return (
+       <BookContext.Consumer>{value=>{
+        return( <section className="page-section" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">BookFolio</h2>
+            <h3 className="section-subheading text-muted">
+              Lorem ipsum dolor sit amet consecte
+              tur.
+            </h3>
+          </div>
+          <div className="row">
+            {value.books.map((book, i) => {
+              return <Book book={book} key={i}></Book>;
+            })}
+          </div>
+        </div>
+      </section>)
+       }}</BookContext.Consumer>
     )
-   
+  }
 }
-} 
-export default Booklist
+export default Booklist;
