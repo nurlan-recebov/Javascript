8f437c49eb12fbe7cc2cd7ff5719100428509b3a
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-08-13 22:57:25 +0400

    Booksfolio

--- Books/Books/src/components/Booklist.jsx
+++ Books/Books/src/components/Booklist.jsx
@@ -9,13 +9,17 @@ class Booklist extends React.Component {
       <ThemeContext.Consumer>
         {(contextTheme) => (
           <BookContext.Consumer>
-              {contextBook => {
-                const{books}=contextBook
-            const {isDarkTheme, dark, light}=contextTheme
-            const theme=isDarkTheme? dark:light;
-          
+            {(contextBook) => {
+              const { books } = contextBook;
+              const { changeTheme, isDarkTheme, dark, light } = contextTheme;
+              const theme = isDarkTheme ? dark : light;
+
               return (
-                <section className="page-section" style={{background:theme.bg,color:theme.txt}} id="portfolio">
+                <section
+                  className="page-section"
+                  style={{ background: theme.bg, color: theme.txt }}
+                  id="portfolio"
+                >
                   <div className="container">
                     <div className="text-center">
                       <h2 className="section-heading text-uppercase">
@@ -24,6 +28,14 @@ class Booklist extends React.Component {
                       <h3 className="section-subheading text-muted">
                         Lorem ipsum dolor sit amet consecte tur.
                       </h3>
+                      <button
+                      
+                        onClick={changeTheme}
+                        type="button"
+                        className="btn btn-sm mb-4 btn-danger"
+                      >
+                        Change theme
+                      </button>
                     </div>
                     <div className="row">
                       {books.map((book, i) => {
