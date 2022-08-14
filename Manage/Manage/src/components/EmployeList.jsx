6c79670e824461a4a8b4c78b410b217151c99d89
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-08-15 01:05:35 +0400

    react-bootstrap

--- Manage/Manage/src/components/EmployeList.jsx
+++ Manage/Manage/src/components/EmployeList.jsx
@@ -1,35 +1,64 @@
-import Employe from "./Employe"
-import { useContext } from "react"
-import   {EmployeContext} from "./contexts/EmployeContext"
-import {Form,Button} from 'react-bootstrap'
-const EmployeList=()=>{
-  const{employes} = useContext(EmployeContext)
-    return( <div>
-        <div className="table-title">
-				<div className="row">
-					<div className="col-sm-6">
-						<h2>Manage <b>Employees</b></h2>
-					</div>
-					<div className="col-sm-6">
-						<Button className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>					
-					</div>
-				</div>
-			</div>
-        <table className="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Address</th>
-						<th>Phone</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-       <Employe employes={employes}/>
-       </tbody>
-       </table>
-       </div>
-    )
-}
-export default EmployeList
+import Employe from "./Employe";
+import { useContext, useState } from "react";
+import { EmployeContext } from "./contexts/EmployeContext";
+import { Button, Modal } from "react-bootstrap";
+
+import AddForm from "./AddForm";
+const EmployeList = () => {
+  const { employes } = useContext(EmployeContext);
+  const [show, setShow] = useState(false);
+  
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
+  return (
+    <>
+      <div className="table-title">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>
+              Manage <b>Employees</b>
+            </h2>
+          </div>
+          <div className="col-sm-6">
+            <Button
+              onClick={handleShow}
+              className="btn btn-success text-white"
+              data-toggle="modal"
+            >
+              <i className="material-icons">&#xE147;</i>{" "}
+              <span>Add New Employee</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Employe employes={employes} />
+        </tbody>
+      </table>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className="modal-header" closeButton>
+          <Modal.Title>Add Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddForm></AddForm>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose} variant="secondary">
+            Close Modal
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+export default EmployeList;
