113b01b881d4b2d327f0a2e2cb7de37fd4e2099c
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-08-23 00:48:54 +0400

    quizapp

--- Quiz App/Quiz App/src/components/App.jsx
+++ Quiz App/Quiz App/src/components/App.jsx
@@ -12,7 +12,7 @@ const App=() =>{
     B:"Ava Max",
     C:"Beyonce",
     D:"Katy Perry",
-    dogru:"A"
+    dogru:"Rihanna"
     },
     {id:2,
       img:'https://tr.web.img4.acsta.net/pictures/19/08/22/10/22/5221340.jpg',
@@ -21,7 +21,7 @@ const App=() =>{
       B:"Ava Max",
       C:"Beyonce",
       D:"Katy Perry",
-      dogru:"D"
+      dogru:"Katy Perry"
       },
       {id:3,
         img:'https://images.theconversation.com/files/478225/original/file-20220809-24-orxjcu.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop',
@@ -30,38 +30,44 @@ const App=() =>{
         B:"Ava Max",
         C:"Beyonce",
         D:"Katy Perry",
-        dogru:"A"
+        dogru:"Beyonce"
         },
         {id:4,
-          img:'https://egscholars.com/wp-content/uploads/2022/02/Dua-Lipa.jpg',
+          img:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DuaLipaO2020522_%28101_of_110%29_%2852052470251%29_%28cropped%29.jpg/1200px-DuaLipaO2020522_%28101_of_110%29_%2852052470251%29_%28cropped%29.jpg',
           sual:"Bu kimdir ?",
           A:"Rihanna",
           B:"Selena Gomez",
-          C:"Dua Lipa",
+          C:"Dua lipa",
           D:"Katy Perry",
-          dogru:"A"
+          dogru:"Dua lipa"
           },
       
      ])
-     
+     const [show, setShow] = useState(0);
      const [count, setCount] = useState(0);
+     const handleShow=()=>{
+      count<asks.length-1 ?setCount(count+1): setCount(count)
+     
+    }
+    const handleFind=(e)=>{
+e.target.innerText===asks[count].dogru ?setShow(show+10): setShow(show-10)
+    }
    return(
     <div className='container'>
       <div className='row justify-content-center'>
-      <div className='col-lg-4 col-sm-6 mb-2'  key={asks[count].id}>
-      <h2 className='text text-white'>{asks[count].sual}</h2>
+      <div className='col-lg-4 col-sm-6'  key={asks[count].id}>
+      <h4 className='text-white'>Puan {show}</h4>
+      <h4 className='text text-white'>{asks[count].sual}</h4>
       <img className='img' src={asks[count].img} alt="" />
-     <div >
-     <button  onClick={()=> count<asks.length-1 ?setCount(count+1): setCount(count)} type="button" className="btn btn-light m-2">{asks[count].A}</button>
-      <button  onClick={()=> count<asks.length-1 ?setCount(count+1): setCount(count)} type="button" className="btn btn-light ml-2 m-2">{asks[count].B}</button>
-     </div>
-<div>
-<button  onClick={()=> count<asks.length-1 ?setCount(count+1): setCount(count)} type="button" className="btn btn-light m-2">{asks[count].C}</button>
-      <button  onClick={()=> count<asks.length-1 ?setCount(count+1): setCount(count)} type="button" className="btn btn-light m-2">{asks[count].D}</button>
+     <div onClick={handleFind} >
+     <button  onClick={handleShow} type="button" className="btn btn-light m-2">{asks[count].A}</button>
+     <button  onClick={handleShow} type="button" className="btn btn-light m-2">{asks[count].B}</button>
+     <button  onClick={handleShow} type="button" className="btn btn-light m-2">{asks[count].C}</button>
+     <button  onClick={handleShow} type="button" className="btn btn-light m-2">{asks[count].D}</button>
 </div>
     </div>
    </div>
-  
+   
      
       </div>
    )
