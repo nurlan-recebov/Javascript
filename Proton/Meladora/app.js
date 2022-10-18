461efb7faa8db69d0593f02dc9c6d2c51819a772
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-10-18 17:54:59 +0400

    firstcommit

--- Proton/Meladora/app.js
+++ Proton/Meladora/app.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector("#navbar")
-const scrol = document.querySelector('.scroll-items')
+const scrol = document.querySelector('.scroll-items');
+const btn = document.querySelector('#btn')
+const search = document.querySelector('#search')
 window.onscroll = function() {
     scrollFunction();
 };
@@ -25,4 +27,30 @@ function scrollFunction() {
         navbar.style.zIndex = '0'
 
     }
+}
+
+function myFunction() {
+    search.classList.toggle('show')
+
+}
+let list = document.querySelectorAll('.list')
+let itembox = document.querySelectorAll('.item-box')
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function() {
+        for (let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active')
+
+        }
+        this.classList.add('active')
+        let dataFilter = this.getAttribute('data-filter')
+        for (let k = 0; k < itembox.length; k++) {
+            itembox[k].classList.remove('active')
+            itembox[k].classList.add('hide')
+            if (itembox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                itembox[k].classList.remove('hide');
+                itembox[k].classList.add('active')
+            }
+        }
+    })
+
 }
