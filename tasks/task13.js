9798248ac148cae274b6888f37389d76aae9192f
Nurlan Recebov <71444150+nurlan-recebov@users.noreply.github.com>
2023-04-30 22:46:37 +0400

    tasks

---
+++ tasks/task13.js
@@ -0,0 +1,20 @@
+const quickSort = (arr) => {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let pivot = arr[0];
+    let leftArr = [];
+    let rightArr = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            leftArr.push(arr[i]);
+        } else {
+            rightArr.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+console.log(quickSort([7, 2, 3, 5, 9]));
